5549d2e11137279c06e2a7f2b0d4346306ad6830
Anmol Mishra <97759302+mishranmol@users.noreply.github.com>
2025-08-21 01:05:20 +0530

    Update GlobalExceptionHandler.java

--- IntroductionToSpringBoot-Running code/IntroductionToSpringBoot/src/main/java/com/anmol/week1/IntroductionToSpringBoot/advices/GlobalExceptionHandler.java
+++ IntroductionToSpringBoot-Running code/IntroductionToSpringBoot/src/main/java/com/anmol/week1/IntroductionToSpringBoot/advices/GlobalExceptionHandler.java
@@ -8,24 +8,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.NoSuchElementException;
 
 
-// When an exception is thrown in any of @RestController, Spring will look for a matching @ExceptionHandler method in classes annotated
-// with @RestControllerAdvice.
-//If any exception is being occurred in any of the controllers then all those exceptions are being handled inside this class hence we have
-// denoted/annotated it with @RestControllerAdvice
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//this below code will handle all the NoSuchElementException occurred in any of the controllers
-
+    
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> NoSuchUserFound(NoSuchElementException e){
 
-    // this below code will work correctly in postman if we try to find a user whose ID is present inside DB but in status code it will
-    // show 200oK so to handle it will use , new ResponseEntity<>("Employee with such ID is not Present",HttpStatus.NOT_FOUND);
-    //  below code is this -> return "Employee of given ID is not Present";
-
-//  The below code is running perfectly But we have commented it because we have return ApiError in response because ApiError has more detail
    return new ResponseEntity<>(e.getMessage() , HttpStatus.NOT_FOUND);
 
 
