d9b2c8dad51c4e4ba395a0f6e5b07f3e77b64cdd
Anmol Mishra <97759302+mishranmol@users.noreply.github.com>
2025-08-20 23:01:00 +0530

    Update EmployeeController.java

--- IntroductionToSpringBoot-Running code/IntroductionToSpringBoot/src/main/java/com/anmol/week1/IntroductionToSpringBoot/controllers/EmployeeController.java
+++ IntroductionToSpringBoot-Running code/IntroductionToSpringBoot/src/main/java/com/anmol/week1/IntroductionToSpringBoot/controllers/EmployeeController.java
@@ -15,35 +15,7 @@ import java.util.Optional;
 @RequestMapping(path = "/employees")
 public class EmployeeController {
 
-//when the service_layer was not used as bridge then we used this code where we have injected the repository directly inside
-// the controller and this was working perfectly but its not recommended
-
-//    private final EmployeeRepository employeeRepository;
-//
-//    public EmployeeController(EmployeeRepository employeeRepository) {
-//        this.employeeRepository = employeeRepository;
-//    }
-//
-//
-//
-//    @GetMapping(path = "/{employeeId}")
-//    public EmployeeEntity getEmployeeById(@PathVariable Long employeeId){
-//    // return new EmployeeDTO(employeeId,"anmol","abc@gmail.com",22,LocalDate.of(2020,1,2),true);
-//        return employeeRepository.findById(employeeId).orElse(null);
-//
-//    }
-//
-//    @GetMapping
-//    public List<EmployeeEntity> getAllEmployees(){
-//        return employeeRepository.findAll();
-//    }
-//
-//    @PostMapping
-//    public EmployeeEntity CreateNewEmployee(@RequestBody EmployeeEntity inputEmployee){
-//        return employeeRepository.save(inputEmployee);
-//    }
-
-
+    
     private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
@@ -52,15 +24,10 @@ public class EmployeeController {
 
 
 
+    //Fetching a particular employee 
     @GetMapping(path = "/{employeeId}")
     public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable Long employeeId){
        Optional<EmployeeDTO> employeeDTO = employeeService.getEmployeeById(employeeId);
-
-// this below code will also work but it will not show the meaningful information in postman if we try to found a user whose ID did not exist
-//       return employeeDTO.
-//               map(employeeDTO1 -> ResponseEntity.ok(employeeDTO1)).
-//               orElse(ResponseEntity.notFound().build());
-
         return employeeDTO.
                map(employeeDTO1 -> ResponseEntity.ok(employeeDTO1)).
                orElseThrow(()-> new NoSuchElementException("No employee exists with given Id "+ employeeId ));
@@ -80,32 +47,34 @@ public class EmployeeController {
     }
 
 
+    
+    //Adding an employee
     @PostMapping
-    //@Valid annotation , this is used to validate all the fields of the EmployeeDTO
-    //how this works , as soon as the control of the program counters @Valid it will NOT go to the EmployeeDTO employeeDTO = employeeService.CreateNewEmployee(inputEmployee);
-    //it will go to the EmployeeDTO for checking the validation of the fields and then the control will come to EmployeeDTO employeeDTO = emp..
-
+    //@Valid annotation is used to validate all the fields of the EmployeeDTO
     public ResponseEntity<EmployeeDTO> CreateNewEmployee(@RequestBody @Valid EmployeeDTO inputEmployee){
         EmployeeDTO employeeDTO = employeeService.CreateNewEmployee(inputEmployee);
-        // write either this line ->  return ResponseEntity.ok(employeeDTO); OR down one
-        // The below one is used to define different types of status codes
         return new ResponseEntity<>(employeeDTO, HttpStatus.CREATED);
     }
 
 
+    
+    //Updating an existing employee
     @PutMapping(path = "/Update/{employeeId}")
     public EmployeeDTO UpdateEmployeeById(EmployeeDTO employeeDTO,@PathVariable Long employeeId){
         return employeeService.UpdateEmployeeById(employeeDTO,employeeId);
     }
 
+
+    
+    //Updating some details of an existing employee
     @PatchMapping(path = {"/UpdatePartial/{employeeId}"})
     public EmployeeDTO UpdatePartialEmployeeById(Map<String,Object> updates , @PathVariable Long employeeId){
-        // we have taken a Map of string,object but you could have also taken <string,string>
         return employeeService.UpdatePartialEmployeeById(updates,employeeId);
     }
 
 
-    //done by own , each and every logic of this below delete method
+    
+    //Deleting an employee
     @DeleteMapping(path = "/delete/{employeeId}")
     public ResponseEntity<Boolean> DeleteEmployeeById(@PathVariable Long employeeId){
         boolean ans = employeeService.DeleteEmployeeById(employeeId);
@@ -117,12 +86,13 @@ public class EmployeeController {
         }
     }
 
-    //done by own
-    //used to delete all the employees from the DB
+  
+    //Deleting all the employees 
     @DeleteMapping(path = "/deleteAll")
     public Boolean deleteAllEmployees() {
         employeeService.deleteAllEmployees();
         return true;
     }
 
+    
 }
