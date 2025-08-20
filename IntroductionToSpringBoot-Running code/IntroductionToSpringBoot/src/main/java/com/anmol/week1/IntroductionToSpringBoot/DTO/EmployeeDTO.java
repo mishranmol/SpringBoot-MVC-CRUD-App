7d021eaaf5e2a0a01de99e149119d6bf8758153a
Anmol Mishra <97759302+mishranmol@users.noreply.github.com>
2025-08-21 01:02:54 +0530

    Update EmployeeDTO.java

--- IntroductionToSpringBoot-Running code/IntroductionToSpringBoot/src/main/java/com/anmol/week1/IntroductionToSpringBoot/DTO/EmployeeDTO.java
+++ IntroductionToSpringBoot-Running code/IntroductionToSpringBoot/src/main/java/com/anmol/week1/IntroductionToSpringBoot/DTO/EmployeeDTO.java
@@ -20,9 +20,9 @@ public class EmployeeDTO {
 
     private Long Id;
 
-    //this is how we can put multiple validation checks on Field="name" 1st check->It should be not empty and 2nd check-> is size/length
+   
     @NotBlank(message = "Name should not be Blank")
-    //it will check the length of string should between 3 and 10 both inclusive
+    //Check the length of string should between 3 and 10 both inclusive
     @Size(min = 3 , max = 10 , message = "Characters should be in Range [3,10]")
     private String name;
 
@@ -37,10 +37,9 @@ public class EmployeeDTO {
     private int age;
 
     @NotBlank(message = "Role should not be blank")
-    //creating our own custom annotation down
+    //Creating our own custom annotation 
     @EmployeeRoleValidationAnnotation
-//  @Pattern(regexp = "^(ADMIN|USER)$" , message = "Role can either be USER or ADMIN")
-    //it means that role can either be "ADMIN" or "USER"
+    //@Pattern(regexp = "^(ADMIN|USER)$" , message = "Role can either be USER or ADMIN")
     private String role; // ADMIN OR USER
 
     @NotNull(message = "Date cannot be null")
@@ -50,15 +49,12 @@ public class EmployeeDTO {
     @NotNull(message = "Salary Should not be Blank")
     @Positive(message = "Salary cannot be negative")
     @Digits(integer = 5 , fraction = 2 , message = "Salary format should be XXXXX.YY")
-    //integer = 5 means no. of digits before the decimal can be UPTO 5 and fraction = 2 means after the decimal it can only be UPTO 2
+    //integer = 5 indicates no. of digits before the decimal can be UPTO 5 and fraction = 2 means after the decimal can only UPTO 2
     @DecimalMin(value = "100.99" , message = "minimum salary should be 100.99")
-    //means minimum value of salary can be 100.99
     @DecimalMax(value = "99999.99" , message = "maximum salary can be 99999.99")
-    //means maximum value of salary can be 99999.99
     private Double salary;
 
-
-//    @AssertTrue(message = "Employee should be True always")
+    
     private boolean isActive;
 
 }
